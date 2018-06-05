1721bd98b08dde305950114d97e17aaef3fc96ae
thewarhammer <joshualwarrick@yahoo.com>
2018-06-04 22:57:54 -0500

    added basics

---
+++ src/main/java/wutang_clan/Application.java
@@ -0,0 +1,22 @@
+package wutangclan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @RequestMapping("/")
+    public String home() {
+        return "C.R.E.A.M!!!! WUTANG CLAN!!!!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
+
